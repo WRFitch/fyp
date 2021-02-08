5612b68380905f7257abe28ac66cefe11d01ffc9
WRFitch <WRFitch@outlook.com>
2021-02-08 15:45:16 +0000

    updated constants and local py scripts

--- src/fyputil/constants.py
+++ src/fyputil/constants.py
@@ -1,6 +1,30 @@
 # pip install earthengine-api
 import ee 
-import constants as c
+
+#==============================================================================
+# FYP Constants 
+#==============================================================================
+
+drive_path = "/content/drive/MyDrive/"
+export_dir = "img_export"
+geotiff_dir = "geotiff_export"
+data_dir = f"{drive_path}{export_dir}"
+png_dir = f"{data_dir}/png"
+
+CO_band = 'CO_column_number_density'
+HCHO_band = 'tropospheric_HCHO_column_number_density'
+NO2_band = 'tropospheric_NO2_column_number_density'
+O3_band = 'O3_column_number_density'
+SO2_band = 'SO2_column_number_density'
+CH4_band = 'CH4_column_volume_mixing_ratio_dry_air'
+ghg_bands = [CO_band, HCHO_band, NO2_band, O3_band, SO2_band, CH4_band]
+
+lon = "longitude"
+lat = "latitude"
+
+#==============================================================================
+# Earth Engine Constants
+#==============================================================================
 
 # Define collections for each dataset to be used 
 # TODO should these be here? are they going to be used for anything?
